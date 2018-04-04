205c60ab564ab326297c0a0f6197fd35aa89ac9f
ramjin.rajin@hotmail.com <ramjin.rajin@hotmail.com>
2018-04-05 00:41:32 +0530

    Web App bug fix

--- KidAccidentPrecaution/Controllers/Product Modules/ContactUsController.cs
+++ KidAccidentPrecaution/Controllers/Product Modules/ContactUsController.cs
@@ -8,9 +8,17 @@ namespace KidAccidentPrecaution.Controllers.Product_Modules
 {
     public class ContactUsController : Controller
     {
-        // GET: ContactUs
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            ViewBag.Message = "Thank you for the query ,We will contact you shortly";
+            ViewBag.Status = "Success";
             return View();
         }
     }
